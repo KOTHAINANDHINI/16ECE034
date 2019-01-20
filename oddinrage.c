3ceeb328dca55bc0fc23fc973d65c615db6efa3c
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-21 04:45:14 +0530

    Create oddinrage.c

---
+++ oddinrage.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    for(i>a;i<b;i++)
+    {
+        if(i!=a&&i!=b)
+        {
+        if(i%2!=0)
+        printf("%d",i);
+        }
+    }
+    getch();
+}
