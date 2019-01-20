ef7f3e2a1a3bb6597076055e860fc49032afcb40
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-21 03:22:39 +0530

    Update vow.c

--- vow.c
+++ vow.c
@@ -4,6 +4,8 @@ void main()
 {
 char a;
 scanf("%c",&a);
+if((a>=65&&a<=90)||(a>=97&&a<=122))
+{  
 if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
 {
 printf("vowel");
@@ -11,6 +13,11 @@ printf("vowel");
 else
 {
 printf("consonant");
+}
+}
+else
+{
+printf("invalid");
 }
 getch();
 }
