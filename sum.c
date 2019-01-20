43f923a196fdcca21ee7d8019073238543dcc399
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-21 03:42:51 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        c=c+i;
+    }
+        printf("%d",c);
+    
+    getch();
+}
