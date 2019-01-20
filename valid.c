f1b43ea0736bd208e7792c90f21b47a9a35ab3da
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-21 03:06:41 +0530

    Create valid.c

---
+++ valid.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a!=0)
+{
+if(a%2==0)
+{
+printf("even");
+}
+if(a%2!=0)
+{
+printf("odd");
+}
+}
+else
+{
+printf("invalid");
+}
+getch();
+}
