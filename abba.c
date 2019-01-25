b543e0c05135e55fc712a49ceff5fb0ec2c33384
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-25 22:27:27 +0530

    Create abba.c

---
+++ abba.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[20],b[20],c[20],d[20];
+    int i,j=0,l,k=0,m=0;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(i==0||i%2==0)
+        {
+            b[j]=a[i];
+            j++;
+            
+        }
+        else
+        {
+            c[k]=a[i];
+            k++;
+            
+        }
+    }
+    m=0;
+    for(i=0;i<j;i++)
+    {
+       for(l=0;l<k;l++)
+       {
+           d[m]=c[l];
+           m++;
+           d[m]=b[i];
+           m++;
+       }
+    }
+    for(i=0;i<m;i++)
+    {
+        printf("%c\n",d[i]);
+    }
+    getch();
+}
