40dc2c107cca37f92f5a87cd8a9f0cdbf7009f93
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-28 22:56:58 +0530

    Update abba.c

--- abba.c
+++ abba.c
@@ -1,6 +1,45 @@
 #include<stdio.h>
 #include<conio.h>
+void main()#include<stdio.h>
+#include<conio.h>
 void main()
+{
+    char a[20],b[20],c[20],d[20];
+    int i,j=0,l,k=0,m=0;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(i==0||i%2==0)
+        {
+            b[j]=a[i];
+            j++;
+            
+        }
+        else
+        {
+            c[k]=a[i];
+            k++;
+            
+        }
+    }
+    m=0;
+    i=0;
+    l=0;
+    while(i!=j&&l!=k)
+       {
+           d[m]=c[l];
+           m++;
+           d[m]=b[i];
+           m++;
+           i++;
+           l++;
+       }
+    for(i=0;i<m;i++)
+    {
+        printf("%c",d[i]);
+    }
+    getch();
+}
 {
     char a[20],b[20],c[20],d[20];
     int i,j=0,l,k=0,m=0;
