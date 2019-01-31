c2f6570ba8d98aaa72d49f227d3d2d4606dfc452
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-31 22:57:18 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,a,b;
+    scanf("%d%d",&a,&b);
+    i=a*b;
+        if (i>0)
+        {
+        if(i%2==0)
+        printf("even");
+        else
+        printf("odd");
+        }
+    getch();
+}
