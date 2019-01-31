5f8f1a447a540c1f2c3b592e03c967c7f8f54f30
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-01 00:02:19 +0530

    Create numeral.c

---
+++ numeral.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[100],i,l=0,m=0;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>='0'&&a[i]<='9')
+        {
+            m++;
+        }
+    }
+    if(m>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
+        
+        
