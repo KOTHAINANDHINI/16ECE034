973007023b1abf5790efdcef9d20254f9317034b
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-01-31 23:20:21 +0530

    Update repeatcount.c

--- repeatcount.c
+++ repeatcount.c
@@ -2,19 +2,16 @@
 #include<conio.h>
 void main()
 {
-    int a[10],i,n,j,k=0;
-    scanf("%d",&n);
+    int a[10],i,n,k=0,m;
+    scanf("%d%d",&n,&m);
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i]==a[j])
+        if(a[i]==m)
             k++;
-        }
     }
     printf("%d",k);
     getch();
