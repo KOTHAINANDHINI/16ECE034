4f047d368f19455fa3ed0b48002cb0f39a6560c8
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-08 22:35:31 +0530

    Create digitsodd.c

---
+++ digitsodd.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,l=0,k[100];
+    scanf("%d",&a);
+    l=a;
+    int i=0;
+    while(l!=0)
+    {
+        
+        k[i]=l%10;
+        l=l/10;
+        i++;
+        
+    }
+    int j;
+    for(j=0;j<i;j++)
+    {
+        if(k[j]%2!=0)
+        {
+            printf("%d",k[j]);
+        }
+        
+    }
+    getch();
+}
