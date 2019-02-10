8bb58efb3ac4679584a83b2c52b56e864506c222
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-10 20:02:18 +0530

    Create 9SET.C

---
+++ 9SET.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a;
+   char c;
+   int b;
+   scanf("%d%c%d",&a,&c,&b);
+   if(c=='/')
+   {
+       printf("%d",a/b);
+   }
+   if(c=='%')
+   {
+       printf("%d",a%b);
+   }
+   getch();
+}
